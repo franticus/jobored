07d859f3c6fef735f48619618c22cf5cbc4e273e
S.Zelensky <zelenstein@gmail.com>
2023-05-12 20:54:51 +0300

    jobored: vacancies link

--- src/components/favorite/Favorite.tsx
+++ src/components/favorite/Favorite.tsx
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom';
 import s from './Favorite.module.scss';
 import notFoundImg from './img/not-found.png';
 
 export const Favorite = () => {
   return (
     <div className={s.favorite}>
-      <img className={s.not_found_img} src={notFoundImg} alt='Not Found' />
-      <p className={s.not_found_text}>Упс, здесь еще ничего нет!</p>
+      <img className={s.notFound_img} src={notFoundImg} alt='Not Found' />
+      <p className={s.notFound_text}>Упс, здесь еще ничего нет!</p>
+      <Link to='/vacancies' className={s.notFound_btn_container}>
+        <button className={s.notFound_btn}>Поиск Вакансий</button>
+      </Link>
     </div>
   );
 };
