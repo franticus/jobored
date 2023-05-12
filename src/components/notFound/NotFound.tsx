edec5b46c9250f975a6b554362f348707e35038a
S.Zelensky <zelenstein@gmail.com>
2023-05-12 21:14:24 +0300

    jobored: notFound

--- src/components/notFound/NotFound.tsx
+++ src/components/notFound/NotFound.tsx
@@ -1,11 +1,11 @@
 import s from './NotFound.module.scss';
-import noFoundImg from './img/no-found.png';
+import notFoundImg from './img/not-found.png';
 
 export const NotFound = () => {
   return (
     <div className={s.notFound}>
-      <img src={noFoundImg} alt='Not Found' />
-      NotFound
+      <img className={s.notFound_img} src={notFoundImg} alt='Not Found' />
+      <p className={s.notFound_text}>Страница не найдена. Она была удалена, либо вовсе не существовала на сайте.</p>
     </div>
   );
 };
